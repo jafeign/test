7f7fed89c44687d1f60e3eb0e76253e23d7624e0
jafeign <89408426+jafeign@users.noreply.github.com>
2024-06-19 11:21:49 +0800

    system commit for vueapp1

---
+++ src_microsites/vueapp1/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="fullscreen-container">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+};
+</script>
+
+<style>
+/* 确保iframe撑满其父容器 */
+.fullscreen-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden; /* 隐藏滚动条 */
+}
+</style>
