5f5ae2ff2427a2350a53c464b5806a3efe8b6365
jafeign <89408426+jafeign@users.noreply.github.com>
2024-06-20 10:49:27 +0800

    system commit for vueapp5

---
+++ src_microsites/vueapp5/vue.config.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+    // // 例如 https://www.baidu.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.baidu.com/admin/，则设置 baseUrl 为 /admin/。
+    publicPath: "/micrositeHtml/app3/",
+    // // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
+    outputDir: 'dist',
+    // // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+    assetsDir: 'static'
+};
